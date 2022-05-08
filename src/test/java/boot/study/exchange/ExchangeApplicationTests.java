70a329accd5ea69e93b9e516e17594a6c4ae7836
rnfkaus8 <kth4350@gmail.com>
2022-05-08 23:34:05 +0900

    add project

---
+++ src/test/java/boot/study/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.study.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
